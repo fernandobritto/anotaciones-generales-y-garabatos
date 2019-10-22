d8025afdca8f7fb609ffd5dfb3dd7bc925bffda6
Fernando Britto <me@fernandobritto.com.br>
2019-10-21 22:10:40 -0200

    Autoload - Code

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '' => array($vendorDir . '/code/php-classes/src'),
+    'Code\\' => array($vendorDir . '/code/php-classes/src'),
 );
