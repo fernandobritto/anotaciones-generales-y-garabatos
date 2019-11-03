33ebb1d4f14a64eeeff5e135bdbb2546de27e18d
Fernando Britto <me@fernandobritto.com.br>
2019-11-03 14:12:01 -0200

    Initial commit

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Store\Page;
+use \Store\Model\User;
 
 
 $app = new Slim();
@@ -18,8 +19,46 @@ $app->get('/', function() {
 
 // admin page
 $app->get('/admin', function(){
+
+	// Chamada do metodo statico que verifica o login
+	User::verifyLogin();
+
+
 	$page = new \Store\PageAdmin();
 	$page->setTpl("index");
+});
+
+
+
+// Login 
+$app->get('/admin/login', function() {
+    
+	$page = new \Store\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+// Validação do Login
+$app->post('/admin/login', function() {
+
+	//User::login(post('login'), post('password'));
+	User::login($_POST['login'], $_POST['password']);
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
 });
 
 
