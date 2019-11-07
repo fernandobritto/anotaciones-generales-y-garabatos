e478552de9b53e3c26154cbac2c90e5c9ff4b990
Fernando Britto <me@fernandobritto.com.br>
2019-11-07 21:06:20 -0200

    Initial commit

---
+++ vendor/code/php-classes/src/Model/User.php
@@ -0,0 +1,81 @@
+<?php 
+
+namespace Store\Model;
+
+use \Store\Model;
+use \Store\DB\Sql;
+
+class User extends Model {
+
+	const SESSION = "User";
+
+	protected $fields = [
+		"id_user", "id_person", "login", "password", "inadmin", "dtergister"
+	];
+
+	public static function login($login, $password):User
+	{
+
+		$db = new Sql();
+
+		$results = $db->select("SELECT * FROM tb_users WHERE login = :LOGIN", array(
+			":LOGIN"=>$login
+		));
+
+		if (count($results) === 0) {
+			throw new \Exception("Não foi possível fazer login.");
+		}
+
+		$data = $results[0];
+
+		if (password_verify($password, $data["password"])) {
+
+			$user = new User();
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		} else {
+
+			throw new \Exception("Não foi possível fazer login.");
+
+		}
+
+	}
+
+	// Medodo statico para efetuar logout
+	public static function logout()
+	{
+
+		$_SESSION[User::SESSION] = NULL;
+
+	}
+
+	// Metodo statico para verificar e validar o login de usuario
+	public static function verifyLogin($inadmin = true)
+	{
+
+		if (
+			!isset($_SESSION[User::SESSION])
+			|| 
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["id_user"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["id_user"] !== $inadmin
+		) {
+			
+			header("Location: /admin/login");
+			exit;
+
+		}
+
+	}
+
+}
+
+
+
+
