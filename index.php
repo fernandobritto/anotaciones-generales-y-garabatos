7fe962c8153b384ff42157ad7dd204943077eb6e
Fernando Britto <me@fernandobritto.com.br>
2019-11-09 09:03:35 -0200

    Admin Template

--- index.php
+++ index.php
@@ -2,16 +2,25 @@
 
 require_once("vendor/autoload.php");
 
+
+use \Store\{Page, PageAdmin};
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Store\Page();
+	$page = new Page();
 
 	$page->setTpl("index");
 
+});
+
+$app->get('/admin', function(){
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
 });
 
 $app->run();
