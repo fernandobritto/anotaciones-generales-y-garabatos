2fd239be22509fd1946bd143cad9a97036dc3bf2
Fernando Britto <me@fernandobritto.com.br>
2019-11-13 21:08:05 -0200

    Login Screen

--- index.php
+++ index.php
@@ -17,12 +17,45 @@ $app->get('/', function() {
 
 });
 
-$app->get('/admin', function(){
-	$page = new PageAdmin();
 
+$app->get('/admin', function() {
+    
+	User::verifyLogin();
+	$page = new PageAdmin();
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('login'), post('password'));
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
 });
 
 $app->run();
 
- ?>
+
+
+
